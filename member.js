cca8b7a6d0917dc1f1602c283f4cf8f740cdcf7a
Yichen Cao <137459906+yichehnc@users.noreply.github.com>
2024-04-17 12:36:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    restrict: 'E',
+    templateUrl; 'modules/skills/views/member.html',
+    controller; 'SkillsMemberController',
+    controllerAs; 'vm',
+    bindToController; true,
+    scope; {
+        skillsMember; 'm'
+    }
+}
